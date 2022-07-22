1a6ea3661852b1ba6e675d5cc113023113dc919e
Aldo Pramilhiawan <aldopramilhiawan0@gmail.com>
2022-07-22 16:37:21 +0700

    baru

---
+++ Mern/client/src/authContext/AuthActions.js
@@ -0,0 +1,16 @@
+export const loginStart = () => ({
+    type: "LOGIN_START",
+});
+
+export const loginSuccess = () => ({
+    type: "LOGIN_SUCCESS",
+});
+
+export const loginFailure = () => ({
+    type: "LOGIN_FAILURE",
+});
+
+// LOGOUT
+export const logout = () => ({
+    type: "LOGOUT",
+  });
